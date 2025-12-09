fe215cf0223df0a7b7ea74ed2afa34d5e2c091f7
Nelia <cornelia.json@hotmail.com>
2025-12-09 17:36:39 +0100

    Basic game setup

--- game.js
+++ game.js
@@ -1,47 +1,22 @@
-import platform from "platform";
-import { Character } from "./character";
-
 function setup() {
-    createCanvas(canvasWidth, canvasHeight);
-}
+  // Create canvas
+  createCanvas(400, 700);
 
-// Obstacle / Spike / Death
-function drawObstacle() {
-    push();
-    fill("red");
-    triangle(180, 300, 210, 240, 240, 300);
-    pop();
+  // Text setup
+  textAlign(CENTER, CENTER);
+  textSize(24);
 }
-
-let canvasWidth = 400;
-let canvasHeight = 400;
-let floor = 300;
-let character = new Character(50, 50, 50, 50);
 
 function draw() {
-    background(100, 100, 100);
+  // Background
+  background(150, 150, 150);
 
-    character.draw();
-    platform.draw();
+  // Ground
+  noStroke();
+  fill(240);
+  rect(0, height - 100, width, 100);
 
-    platform.x -= 10;
-    if (platform.x + platform.w < 0) {
-        platform.x = 500;
-    }
-
-    if (
-        character.y + character.h < 300 &&
-        !character.isColliding(character, platform)
-    ) {
-        character.y += 10;
-    }
-
-    // Floor
-    line(0, floor, canvasWidth, floor);
-}
-
-function keyPressed() {
-    if (character.y + character.h === floor || character.isColliding(character, platform)) {
-        character.y -= 120;
-    }
+  // Title text
+  fill(0);
+  text("Doodle Jump", width / 2, height / 2 - 300);
 }
