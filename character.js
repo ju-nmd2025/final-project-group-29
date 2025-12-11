c338116967f20493c535119c6bf6dc006a5eeb73
Nelia <cornelia.json@hotmail.com>
2025-12-11 12:00:03 +0100

    Character basics

--- character.js
+++ character.js
@@ -1,22 +1,54 @@
-export default class Character {
+// Player class (snowball)
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
 
-    	constructor(x, y, w, h) {
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-//		this.isOnPlatForm = false;
-	}
+    this.radius = 20; // size
+    this.vx = 0;
+    this.vy = 0;
 
-    draw() {
-        rect(this.x, this.y, this.w, this.h);
-    }
+    this.speed = 4; // speed movement left/right
+    this.gravity = 0.5; // speed falling down
+    this.jumpPower = -10; // how strong the jump is
+  }
 
-    isColliding(character, platform) {
-    if (platform.y === character.y + character.w && platform.x <= character.x + character.w) {
-        return true;
+  update() {
+    // --- Horizontal movement ---
+    if (keyIsDown(LEFT_ARROW)) {
+      this.vx = -this.speed;
+    } else if (keyIsDown(RIGHT_ARROW)) {
+      this.vx = this.speed;
     } else {
-        return false;
+      this.vx = 0;
     }
-}
+
+    this.x += this.vx;
+
+    // --- Vertical movement ---
+    // Apply gravity
+    this.vy += this.gravity; // fall down
+    this.y += this.vy;
+
+    const groundY = height - 40 - this.radius;
+
+    if (this.y > groundY) {
+      this.y = groundY;
+      this.vy = 0; // stop falling when on the ground
+    }
+  }
+
+  jump() {
+    const groundY = height - 40 - this.radius;
+    if (this.y >= groundY) {
+      this.vy = this.jumpPower;
+    }
+  }
+
+  draw() {
+    // Draw snowball
+    noStroke();
+    fill(255);
+    circle(this.x, this.y, this.radius * 2);
+  }
 }
